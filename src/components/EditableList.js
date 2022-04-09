0ed7ca55f5c5f1b2ca30ed3ed14f7c8501a202ca
heyyayesh <biswalayesh3@gmail.com>
2022-04-09 11:22:31 +0530

    Refactor to separate an editable list component

---
+++ src/components/EditableList.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { nanoid } from 'nanoid';
+import styles from './editableList.module.css';
+
+function EditableList(props) {
+  const [listItems, setListItems] = React.useState(props.listItems);
+  const [newInputVisible, setNewInputVisible] = React.useState(false);
+
+  const handleListEnter = () => {
+    const btn = document.getElementById('plus');
+    btn.classList.remove(styles.invisible);
+  }
+
+  const handleListLeave = () => {
+    const btn = document.getElementById('plus');
+    btn.classList.add(styles.invisible);
+    setNewInputVisible(false);
+  }
+
+  const handleItemEnter = (id) => {
+    const btn = document.querySelector(`button[id=${id}]`);
+    btn.classList.remove(styles.invisible);
+  }
+
+  const handleItemLeave = (id) => {
+    const btn = document.querySelector(`button[id=${id}]`);
+    btn.classList.add(styles.invisible);
+  }
+
+  const handleAddItemClick = () => {
+    setNewInputVisible(true);
+  }
+
+  const handleNewInputEnter = (e) => {
+    if (e.key === 'Enter') {
+      if (!e.target.value) return;
+      setListItems(prev => [...prev, e.target.value]);
+      setNewInputVisible(false);
+    }
+  }
+
+  const handleItemdelete = (skill) => {
+    setListItems(prev => {
+      return prev.filter(item => {
+        return item !== skill;
+      })
+    })
+  }
+
+  const ul = (
+    <ul key={nanoid()}>
+      {
+        listItems.map((li, index) => {
+          const id = nanoid();
+          return (
+            <li key={index} id={id} onMouseEnter={() => handleItemEnter(id)} onMouseLeave={() => handleItemLeave(id)}>
+              {li}
+              <button id={id} className={styles.invisible} onClick={() => handleItemdelete(li)}><i className="fa-solid fa-minus"></i></button>
+            </li>
+          )
+        })
+      }
+    </ul>
+  );
+
+  console.log(listItems);
+  return (
+    <div className={styles.EditableList} onMouseEnter={handleListEnter} onMouseLeave={handleListLeave}>
+      <h2>{props.heading}</h2>
+      {ul}
+      <button id='plus' className={`${styles.addItem} ${styles.invisible}`} onClick={handleAddItemClick}><i className="fa-solid fa-plus"></i></button>
+      {newInputVisible && <input className={styles.newItemInput} onKeyDown={(e) => handleNewInputEnter(e)} autoFocus />}
+    </div>
+  )
+}
+
+export default EditableList;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,75 +1,10 @@
 import React from 'react';
-import styles from './skills.module.css';
-import { nanoid } from 'nanoid';
+import EditableList from './EditableList';
 
 function Skills() {
-  const [skills, setSkills] = React.useState(['HTML5', 'CSS', 'Javascript']);
-  const [newInputVisible, setNewInputVisible] = React.useState(false);
-
-  const handleSkillsEnter = () => {
-    const btn = document.getElementById('plus');
-    console.log(btn);
-    btn.classList.remove(styles.invisible);
-  }
-
-  const handleSkillsLeave = () => {
-    const btn = document.getElementById('plus');
-    btn.classList.add(styles.invisible);
-    setNewInputVisible(false);
-  }
-
-  const handleListEnter = (id) => {
-    const btn = document.querySelector(`button[id=${id}]`);
-    btn.classList.remove(styles.invisible);
-  }
-
-  const handleListLeave = (id) => {
-    const btn = document.querySelector(`button[id=${id}]`);
-    btn.classList.add(styles.invisible);
-  }
-
-  const handleAddItemClick = () => {
-    setNewInputVisible(true);
-  }
-
-  const handleNewInputEnter = (e) => {
-    if (e.key === 'Enter') {
-      setSkills(prevSkills => [...prevSkills, e.target.value]);
-      setNewInputVisible(false);
-    }
-  }
-
-  const handleItemdelete = (skill) => {
-    setSkills(prev => {
-      return prev.filter(item => {
-        return item !== skill;
-      })
-    })
-  }
-
-  const ul = (
-    <ul>
-      {
-        skills.map((skill) => {
-          const id = nanoid();
-          return (
-            <li key={id} id={id} onMouseEnter={() => handleListEnter(id)} onMouseLeave={() => handleListLeave(id)}>
-              {skill}
-              <button id={id} className={styles.invisible} onClick={() => handleItemdelete(skill)}><i className="fa-solid fa-minus"></i></button>
-            </li>
-          )
-        })
-      }
-    </ul>
-  )
-
+  const skillsArr = ['Html', 'Css', 'Javascript'];
   return (
-    <div className={styles.Skills} onMouseEnter={handleSkillsEnter} onMouseLeave={handleSkillsLeave}>
-      <h2>Skills</h2>
-      {ul}
-      <button id='plus' className={`${styles.addItem} ${styles.invisible}`} onClick={handleAddItemClick}><i className="fa-solid fa-plus"></i></button>
-      {newInputVisible && <input className={styles.newItemInput} onKeyDown={(e) => handleNewInputEnter(e)} autoFocus />}
-    </div>
+    <EditableList heading="Skills" listItems={skillsArr}/>
   )
 }
 
